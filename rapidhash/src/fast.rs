18ae9fc6c0943080e4a49fd1003786e64fc13ad2
Liam Gray <hoxxep@gmail.com>
2025-07-30 13:53:55 +0100

    Default COMPACT on wasm targets, better WASM benchmarks

--- rapidhash/src/fast.rs
+++ rapidhash/src/fast.rs
@@ -5,12 +5,12 @@
 //! This is a specific instantiation of the [rapidhash::inner] module with the following settings:
 //! - `AVALANCHE` is disabled.
 //! - `SPONGE` is enabled.
-//! - `COMPACT` is disabled.
+//! - `COMPACT` is disabled, unless building for WASM targets.
 //! - `PROTECTED` is disabled.
 
 const AVALANCHE: bool = false;
 const SPONGE: bool = true;
-const COMPACT: bool = false;
+const COMPACT: bool = cfg!(target_family = "wasm");
 const PROTECTED: bool = false;
 
 use crate::inner;
